50ea40d45f16a53dc1225c86292c7171257f3578
Master <agnetdesign@gmail.com>
2021-03-21 00:57:43 +0400

    [integration]-second modification

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+
 def add(x,y):
 	return x+y
 
-num1 = int(input("Enter Num1: "))
+def main():
+	num1 = int(input("Enter Num1: "))
 
-num2 = int(input("Enter Num2: "))
+	num2 = int(input("Enter Num2: "))
 
-z = add(num1,num2)
+	z = add(num1,num2)
 
-print("The SUM : ",z)
+	print("The SUM : ",z)
+
+main()
